80d794c855d05a31d4440b3e35b896b99d13f9d2
Thomas Lovén <thomasloven@gmail.com>
2018-09-04 22:55:35 +0200

    A bit of code cleanup

--- long-press.js
+++ long-press.js
@@ -2,7 +2,6 @@ class LongPress extends Polymer.Element {
 
   ready() {
     super.ready();
-    document.addEventListener('click', (e) => this.click(e), true);
     document.addEventListener('mousedown', (e) => this.mouseDown(e));
     document.addEventListener('touchstart', (e) => this.mouseDown(e));
     document.addEventListener('mouseup', (e) => this.mouseUp(e));
@@ -11,83 +10,64 @@ class LongPress extends Polymer.Element {
 
   setConfig(config) {
     this._config = config;
+
     let tag = config.child.type;
     if(tag.startsWith("custom:"))
       tag = tag.substr(7);
     else
       tag = `hui-${tag}-element`;
 
+
+    this.entity = config.child.entity;
+
+    this.enabled = true;
+    this.timer = null;
+
     this.child = document.createElement(tag);
     this.child.setConfig(config.child);
     this.appendChild(this.child);
-
-    this.timer = null;
-    this.held = false;
   }
 
   set hass(hass) {
     this.child.hass = hass;
-    this._hass = hass;
+    this.enabled = true;
     if(hass.moreInfoEntityId) {
-      this.held = false;
-      if(this.timer) {
-        clearTimeout(this.timer);
-        this.timer = null;
-      }
+      this.enabled = false;
+      this._reset();
     }
   }
 
-  click(e) {
-    if (this._hass.moreInfoEntityId) {
-      if(this.held)
-        e.stopPropagation();
-      this.held = false;
-      return;
-    }
-    let rect = this.getBoundingClientRect();
-    let ev = e;
-    if(e.touches)
-      e = e.touches[0];
-    if( e.clientX < rect.left || e.clientX > rect.right || e.clientY < rect.top || e.clientY > rect.bottom)
-      return;
-    if(this.held) {
-      ev.stopPropagation();
-    }
-    this.held = false;
-  }
-
-  mouseDown(e) {
-    if (this._hass.moreInfoEntityId) return;
-    let rect = this.getBoundingClientRect();
-    let ev = e;
-    if(e.touches)
-      e = e.touches[0];
-    if( e.clientX < rect.left || e.clientX > rect.right || e.clientY < rect.top || e.clientY > rect.bottom)
-      return;
-
-    ev.stopPropagation();
-    this.held = false;
-    this.timer = setTimeout((e) => this.onHold(), 300, e);
-  }
-
-  mouseUp(e) {
-    if (this._hass.moreInfoEntityId) return;
+  _reset() {
     if(this.timer) {
       clearTimeout(this.timer);
       this.timer = null;
     }
   }
 
-  onHold() {
-    this.held = true;
+  mouseDown(ev) {
+    if (!this.enabled) return;
+    let rect = this.getBoundingClientRect();
+    let cx = (ev.touches)? ev.touches[0].clientX : ev.clientX;
+    let cy = (ev.touches)? ev.touches[0].clientY : ev.clientY;
+    if( cx < rect.left || cx > rect.right || cy < rect.top || cy > rect.bottom)
+      return;
 
+    this.timer = setTimeout((e) => this.onHold(), 300);
+  }
+
+  mouseUp(e) {
+    if (!this.enabled) return;
+    this._reset();
+  }
+
+  onHold() {
     // Only open more-info dialog for now
-    const entityId = this._config.child.entity;
     let ev = new Event('hass-more-info', {
       bubbles: true,
       cancelable: false,
       composed: true,
     });
+    const entityId = this.entity;
     ev.detail = { entityId };
     this.dispatchEvent(ev);
   }
