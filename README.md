252428b4989b075fc696108d8daddd2b155c5f72
Thomas Lovén <thomasloven@gmail.com>
2018-09-05 20:29:05 +0200

    Allow more actions on long-press. Resolves #1

--- README.md
+++ README.md
@@ -2,26 +2,79 @@
 
 Long press on a picture-elements element to open the more-info dialog.
 
-I haven't tested, but it should work with
+`long-press` wraps any element in a `picture-elements` card and allows one
+action to be taken for a short click or tap, and another for a longer
+click/hold (longer than 300 ms).
 
-- state-badge
-- state-icon
-- state-label
-- icon
-- image
+Possible secondary actions are
 
-And probably most custom elements that have an `entity` property.
+- show the More Info dialog for the entity of the wrapped element
+- show the More Info dialog for a given entity
+- Navigate to URL path
+- Call service
+
+## Options
+
+| Name | Type | Default | Description
+| ---- | ---- | ------- | -----------
+| child | object | **Required** | The wrapped element
+| style | object | **Required** | Position and style of the element
+| ---- | ---- | ------- | -----------
+| entity | string | none | Select entity for More Info dialog
+| navigation\_path | string | none | URL path to navigate to
+| service | string | none | Service to call
+| service\_data | object |   | The service data to use
+
+
+Which action is taken depends on whether the options `entity`,
+`navigation_path` and `service` are defined.
+
+## Example configuration
 
 ```yaml
+# A normal click on any state-icon below will toggle light.my_lamp
   - type: picture-elements
     image: ...
     elements:
+# This will show the More Info dialog for light.my_lamp on long press
     - type: custom:long-press
       child:
         type: state-icon
         entity: light.my_lamp
         tap_action: toggle
       style:
+        left: 0%
+        top: 50%
+# This will show the More Info dialog for light.another_lamp on long press
+    - type: custom:long-press
+      entity: light.another_lamp
+      child:
+        type: state-icon
+        entity: light.my_lamp
+        tap_action: toggle
+      style:
+        left: 25%
+        top: 50%
+# This will navigate to /lovelace/lights on long press
+    - type: custom:long-press
+      navigation_path: /lovelace/lights
+      child:
+        type: state-icon
+        entity: light.my_lamp
+        tap_action: toggle
+      style:
         left: 50%
+        top: 50%
+# This will write an error to the hass log on long press
+    - type: custom:long-press
+      service: system_log.write
+      service_data:
+        message: "A long press happened"
+      child:
+        type: state-icon
+        entity: light.my_lamp
+        tap_action: toggle
+      style:
+        left: 75%
         top: 50%
 ```
--- long-press.js
+++ long-press.js
@@ -2,24 +2,20 @@ class LongPress extends Polymer.Element {
 
   ready() {
     super.ready();
-    document.addEventListener('click', (e) => this._reset());
     document.addEventListener('mousedown', (e) => this.mouseDown(e, false));
     document.addEventListener('touchstart', (e) => this.mouseDown(e, true));
     document.addEventListener('mouseup', (e) => this.mouseUp(e, false));
     document.addEventListener('touchend', (e) => this.mouseUp(e, true));
+    document.addEventListener('click', (e) => this._reset());
   }
 
   setConfig(config) {
     this._config = config;
-
     let tag = config.child.type;
     if(tag.startsWith("custom:"))
       tag = tag.substr(7);
     else
       tag = `hui-${tag}-element`;
-
-
-    this.entity = config.child.entity;
 
     this.enabled = true;
     this.timer = null;
@@ -27,9 +23,14 @@ class LongPress extends Polymer.Element {
     this.child = document.createElement(tag);
     this.child.setConfig(config.child);
     this.appendChild(this.child);
+
+    this.cover = document.createElement('div');
+    this.cover.setAttribute("style", "position: absolute; top: 0; left: 0; width: 100%; height: 100%; visibility: hidden;");
+    this.appendChild(this.cover);
   }
 
   set hass(hass) {
+    this._hass = hass;
     this.child.hass = hass;
     this.enabled = true;
     if(hass.moreInfoEntityId) {
@@ -43,40 +44,57 @@ class LongPress extends Polymer.Element {
       clearTimeout(this.timer);
       this.timer = null;
     }
-    if(document.querySelector('home-assistant').$$('ha-more-info-dialog'))
-      document.querySelector('home-assistant').$$('ha-more-info-dialog').removeAttribute('modal');
+    let dlg = document.querySelector('home-assistant').$$('ha-more-info-dialog');
+    if(dlg)
+      dlg.removeAttribute('modal');
+    this.cover.style.visibility = 'hidden';
   }
 
   mouseDown(ev, touch) {
+    this._reset();
+
     if (!this.enabled) return;
     if(!touch && ev.button != 0) return;
-    let rect = this.getBoundingClientRect();
+
+    let br = this.getBoundingClientRect();
     let cx = (touch)? ev.touches[0].clientX : ev.clientX;
     let cy = (touch)? ev.touches[0].clientY : ev.clientY;
-    if( cx < rect.left || cx > rect.right || cy < rect.top || cy > rect.bottom)
+    if( cx < br.left || cx > br.right || cy < br.top || cy > br.bottom)
       return;
-
-    this._reset();
 
     this.timer = setTimeout((e) => this.onHold(), 300);
   }
 
-  mouseUp(e, touch) {
-    if (!this.enabled) return;
+  mouseUp(ev, touch) {
+    if(!this.enabled) return;
     this._reset();
   }
 
   onHold() {
-    // Only open more-info dialog for now
-    let ev = new Event('hass-more-info', {
-      bubbles: true,
-      cancelable: false,
-      composed: true,
-    });
-    const entityId = this.entity;
-    ev.detail = { entityId };
-    this.dispatchEvent(ev);
-    document.querySelector('home-assistant').$$('ha-more-info-dialog').setAttribute('modal', true);
+    if(this._config.navigation_path) {
+      history.pushState(null, null, this._config.navigation_path);
+      let ev = new Event('location-changed', {
+        bubbles: true,
+        cancelable: false,
+        composed: true,
+      });
+      this.dispatchEvent(ev);
+    } else if (this._config.service) {
+      const [domain, service] = this._config.service.split('.', 2);
+      this._hass.callService(domain, service, this._config.service_data);
+      this.cover.style.visibility='visible';
+    } else {
+      // Only open more-info dialog for now
+      let ev = new Event('hass-more-info', {
+        bubbles: true,
+        cancelable: false,
+        composed: true,
+      });
+      const entityId = this._config.entity || this._config.child.entity;
+      ev.detail = { entityId };
+      this.dispatchEvent(ev);
+      document.querySelector('home-assistant').$$('ha-more-info-dialog').setAttribute('modal', true);
+    }
   }
 }
 
